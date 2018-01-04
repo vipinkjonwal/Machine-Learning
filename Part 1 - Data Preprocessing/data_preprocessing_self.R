6f0c177c0cddebda2e310ce1b9a118e393e3c76c
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-04 20:04:30 +0530

    Data Preprocessing using R

---
+++ Part 1 - Data Preprocessing/data_preprocessing_self.R
@@ -0,0 +1,19 @@
+# Data Preprocessing
+
+# We don't need to import any libraries because they're already present in the
+# 'Packages' tab at right hand side of RStudio, just tick the checklist. However, 
+# we can still add import commands to import libraries.
+
+# library(datasets)
+# This is statement used to import datasets library.
+
+# Importing Datasets
+
+dataset = read.csv('Data.csv')
+# read.csv is function used to import dataset and we have imported the dataset
+# into a user defined variable named 'dataset'.
+
+# NOTE : R indexed start with 1.
+
+# In R, we don't need to distinguish matrix features and dependent-independent 
+# variables.
