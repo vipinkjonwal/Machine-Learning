9db508d8713be6517e23858d0ff4a187315dcfa9
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-15 13:21:28 +0530

    Splitting the Dataset in Python

---
+++ Part 1 - Data Preprocessing/splitingDatasets.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 12:35:50 2018
+
+@author: Vipin Kumar
+"""
+
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing Datasets
+dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[: ,: -1].values
+Y = dataset.iloc[: , -1].values
+
+# Handling missing values
+from sklearn.preprocessing import Imputer
+
+# Creating an object of class Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+
+# Fit the data matrix/values.
+imputer = imputer.fit(X[: , 1:3])
+X[:, 1:3] = imputer.transform(X[: , 1:3])
+
+# Encoding Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+X[: ,0] = labelEncoder_X.fit_transform(X[: ,0])
+
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_Y = LabelEncoder()
+Y = labelEncoder_Y.fit_transform(Y)
+
+# Splitting the Dataset
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y ,test_size = 0.2, random_state = 0)
+
+# test_size tells the % of data we used for test, usually it's 0.2 (20%)
+# 0.25 (25%). Test size shouldn't be 0.5 or more (50% or more).
+
+# train_test_split splits the datasets into two sets (Train and Test Set)
+
+
+
